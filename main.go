fdd71c21fdfdcf9bd61c096e78f9a13a10b91ecd
[a610305] Daniel Tischer <daniel.tischer@atos.net>
2018-06-08 12:07:18 +0200

    add quiet mode

--- main.go
+++ main.go
@@ -11,60 +11,72 @@ import (
 	"strings"
 )
 
-const PROG_NAME string = "healthcheck"
-
-// http://technosophos.com/2014/06/11/compile-time-string-in-go.html
 var version string
 
-// command line flags
-var showVersion bool
-var httpURL string
-var str string
+var flagQuiet = flag.Bool("quiet", false, "do not print anything to console")
+var flagVersion = flag.Bool("version", false, "print version and exit")
 
 func init() {
-	flag.BoolVar(&showVersion, "version", false, "print version and exit")
-	flag.StringVar(&httpURL, "URL", "", "URL of the server to check")
-	flag.StringVar(&str, "string", "", "string to search for in the HTTP response")
+	flag.BoolVar(flagQuiet, "q", false, "")
+	flag.BoolVar(flagVersion, "v", false, "")
 }
 
 func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s <URL> [text to search for in HTTP response]\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
 	log.SetFlags(0)
 	flag.Parse()
 
-	if showVersion {
-		fmt.Printf("%s version %s\n", PROG_NAME, version)
-	} else {
-		if httpURL == "" {
-			flag.Usage()
-			os.Exit(1)
-		}
-		healthCheck(httpURL, str)
+	if flag.Arg(0) == "version" || *flagVersion {
+		fmt.Println("hugo-search version", version)
+		return
 	}
+
+	if flag.NArg() < 1 || flag.NArg() > 2 {
+		flag.Usage()
+		os.Exit(1)
+	}
+	healthCheck(flag.Arg(0), flag.Arg(1))
 }
 
 func healthCheck(url string, text string) {
 	resp, err := http.Get(url)
 	if err != nil {
-		log.Fatalln("Connection error:", err)
+		fatal("Connection error:", err)
 	} else {
 		if resp.StatusCode < 400 {
-			log.Println(url, ": Online - response:", resp.StatusCode)
+			info(url, ": Online - response:", resp.StatusCode)
 			if text != "" {
-				checkHttpBody(resp.Body, text)
+				checkHttpBody(url, resp.Body, text)
 			}
 		} else {
-			log.Fatalln(url, ": Error - response:", resp.StatusCode)
+			fatal(url, ": Error - response:", resp.StatusCode)
 		}
 	}
 }
 
-func checkHttpBody(body io.ReadCloser, s string) {
+func checkHttpBody(url string, body io.ReadCloser, text string) {
 	defer body.Close()
 	bodyBytes, err := ioutil.ReadAll(body)
 	if err != nil {
-		log.Fatalln(httpURL, "Error reading HTTP body:", err)
+		fatal(url, ": Error reading HTTP body:", err)
 	}
-	if !strings.Contains(string(bodyBytes), s) {
-		log.Fatalln(httpURL, "Error - string not found in HTTP response:", s)
+	if !strings.Contains(string(bodyBytes), text) {
+		fatal(url, ": Error - string not found in HTTP response:", text)
+	}
+}
+
+func info(v ...interface{}) {
+	if !*flagQuiet {
+		log.Println(v...)
+	}
+}
+
+func fatal(v ...interface{}) {
+	if !*flagQuiet {
+		log.Fatalln(v...)
 	}
 }
